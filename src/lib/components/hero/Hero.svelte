5e8c86b50f682209dba1a86132a57047b55a9854
jhtorp <jhtorp@hotmail.com>
2023-03-10 11:07:44 +0100

    bakgrunnsfilm riktig plassert

---
+++ src/lib/components/hero/Hero.svelte
@@ -0,0 +1,30 @@
+<script>
+	import HeroNav from '$lib/components/hero/HeroNav.svelte';
+	import HeroText from '$lib/components/hero/HeroText.svelte';
+
+	import backgroundVideo from '$lib/assets/innlandet-piano-bg-video.mp4';
+</script>
+
+<div class="container">
+	<video class="video-bg" autoplay muted loop playsinline>
+		<source src={backgroundVideo} type="video/mp4" />
+	</video>
+</div>
+
+<style>
+	.container {
+		min-height: 100vh;
+		position: relative;
+		overflow: hidden;
+		width: 100%;
+	}
+
+	.video-bg {
+		width: 100%;
+		min-height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		object-fit: cover;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../styles.css';
+
+	import Hero from '$lib/components/hero/Hero.svelte';
+</script>
+
+<section class="hero-section">
+	<Hero />
+</section>
